c6cdd4939dd6cd3ff9f5e8c00a27c246a03baea4
valer-yukhta <v.yukhta@mail.ua>
2016-10-03 20:49:18 +0300

    action.php

--- php_basics_tasks/15/action.php
+++ php_basics_tasks/15/action.php
@@ -3,10 +3,7 @@ include '15.php';
 $operator = $_POST ['operator'];
 $a = $_POST ['a'];
 $b = $_POST ['b'];
-//echo $a;
-//echo $operator;
-//echo $b;
-//var_dump($operator);
+
 if ($b==0 and $operator=='/'){
     echo "на 0 делить нельзя";
 }else {
